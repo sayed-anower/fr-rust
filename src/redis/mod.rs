3afc5845c8cba08d7b7a04f6c3ac093b571e17d1
SAYED ANOWER <sayed.anower.17.2@gmail.com>
2026-05-31 22:25:32 +0600

    Update mod.rs

--- src/redis/mod.rs
+++ src/redis/mod.rs
@@ -1,5 +1,5 @@
 pub mod redis;
-pub use redis::{RedisManager, RedisConfig};
+pub use redis::{RedisManager};
 
 // Re-export AsyncCommands from the external redis crate
 pub use ::redis::AsyncCommands;
