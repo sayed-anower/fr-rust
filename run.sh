70ab3a9c224191a27014176a6ed30a6de0da83ea
SAYED ANOWER <sayed.anower.17.2@gmail.com>
2026-05-31 14:49:08 +0600

    Uncomment Rust installation command in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 # LINUX OR MACOS
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustc --version
 cargo --version
+cargo clean
+cargo check
+cargo build
 cargo fmt
 cargo run
